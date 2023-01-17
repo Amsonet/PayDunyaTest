a362d75dd95554db29e7ff137cb9cf854b6ab1ac
Amzath BOURAIMA <amzath.bouraima@gmail.com>
2023-01-17 16:23:43 +0100

    modifications Dashbord

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
 
@@ -36,6 +34,8 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 });*/
 
 Route::get('/dashboard', [App\Http\Controllers\DashbordController::class, 'index'])->name('dashbord');
+
+Route::get('/', [App\Http\Controllers\NewTransactionController::class, 'index'])->name('home');
 
 Route::get('/home', [App\Http\Controllers\NewTransactionController::class, 'index'])->name('home');
 Route::get('/blank', [App\Http\Controllers\NewTransactionController::class, 'index'])->name('home');
